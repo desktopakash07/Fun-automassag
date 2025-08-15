373432cb0b2e1de23930868075c6f5e13247257d
desktopakash07 <desktopakash07@gmail.com>
2025-08-15 09:17:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Dummy Telegram credentials (for educational/demo purpose only)
+api_id = 25547287
+api_hash = 'b79197bb0c7badbcfe5cc12c9fb9dbc4'
+phone_number = '+918787680944'
+
+# Target Telegram group (fake/demo)
+target_group = '@FUNToken_OfficialChat'
